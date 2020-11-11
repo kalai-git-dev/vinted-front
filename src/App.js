cc8acf02906b2b8fdaeda176197f1acd2cf37917
kalai <mac@MacBook-Pro-de-mac.local>
2020-11-11 14:32:36 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+function App() {
+  return <h1>hello router</h1>;
+}
+
+export default App;
